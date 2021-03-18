a49df5040709c551163814c6f3bb120bfea95faf
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:43:41 +0500

    Create python_in_one_hour_functions.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_functions.py
@@ -0,0 +1,10 @@
+#https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+def print_hello(name='tom'):
+    print(f"Hello, {name}!")
+print_hello()
+print_hello('mike')
+
+def add_numbers(x,y):
+    return x + y
+sum = add_numbers(5,10)
+print(sum)
