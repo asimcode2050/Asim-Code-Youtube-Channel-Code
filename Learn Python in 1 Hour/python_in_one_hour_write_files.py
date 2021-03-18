50bbbcd1aebabc9c22f354808d89751cf5160b53
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:49:12 +0500

    Create python_in_one_hour_write_files.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_write_files.py
@@ -0,0 +1,4 @@
+# https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+filename = '/home/asimcode/python/info.txt'
+with open(filename,'a') as fo:
+    fo.write("\nI love Java programming too.")
