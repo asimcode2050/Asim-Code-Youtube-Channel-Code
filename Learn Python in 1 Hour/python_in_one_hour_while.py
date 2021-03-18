25be2ec1d8758844938fbf1f5260c48fb08b2a0f
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:48:25 +0500

    Create python_in_one_hour_while.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_while.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+value = 1
+while value <= 5:
+    print(value)
+    value += 1
+msg = ''
+while msg != 'quit':
+    msg = input("Please enter your message: ")
+    print(msg)
