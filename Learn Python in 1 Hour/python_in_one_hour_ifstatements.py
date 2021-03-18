724a03fef743ea2ba8135f34f04e3fd7f12b4d15
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:45:00 +0500

    Create python_in_one_hour_ifstatements.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_ifstatements.py
@@ -0,0 +1,13 @@
+#https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+age = 25
+if age >= 18:
+    print("You can vote!")
+
+if age < 4:
+    ticket_price = 0
+elif age < 18:
+    ticket_price = 10
+else:
+    ticket_price = 20
+
+print(ticket_price)
