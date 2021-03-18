965add78b795fc3d21a195c14e51fa04957f3e76
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:39:15 +0500

    Create python_in_one_hour_classes.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_classes.py
@@ -0,0 +1,17 @@
+class Dog():
+    def __init__(self, name):
+        self.name = name
+    def sit(self):
+        print(f"{self.name} is sitting.")
+my_dog = Dog('Max')
+#print(f"Name: {my_dog.name}")
+#my_dog.sit()
+class GoodDog(Dog):
+    def __init__(self,name):
+        super().__init__(name)
+    def bark(self):
+        print("wuff wuff")
+good_boy = GoodDog('Buddy')
+print(f"{good_boy.name} is a good boy")
+good_boy.sit()
+good_boy.bark()
