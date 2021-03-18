a842da7f31a5f2fde7e60ebab901d9057ec26b59
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:45:48 +0500

    Create python_in_one_hour__input.py

---
+++ Learn Python in 1 Hour/python_in_one_hour__input.py
@@ -0,0 +1,7 @@
+# https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+name = input("Please enter your name:")
+print(f"Hello, {name}!")
+age = input("How old are you ?")
+age = int(age)
+pi = input("What is the value of pi ?")
+pi = float(pi)
