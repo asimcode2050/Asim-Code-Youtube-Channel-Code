da7e5521f1552ed57a1a3a61f883e2c9f7f4a27f
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:47:33 +0500

    Create python_in_one_hour_var_strings.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_var_strings.py
@@ -0,0 +1,8 @@
+# https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+print("Hello world!")
+hello_msg = "Hello world!"
+print(hello_msg)
+first_name = "Asim"
+last_name = "Iqbal"
+full_name = f"{first_name} {last_name}"
+print(full_name)
