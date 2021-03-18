023442feeb3140529c7b5796479c0d6bf92a077d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-03-18 22:46:42 +0500

    Create python_in_one_hour_lists.py

---
+++ Learn Python in 1 Hour/python_in_one_hour_lists.py
@@ -0,0 +1,22 @@
+# https://www.youtube.com/watch?v=z_eALBTtQTk&ab_channel=AsimCode
+names = ['tom','peter','john']
+first_item = names[0]
+print(first_item)
+last_name = names[-1]
+print(last_name)
+for name in names:
+    print(name)
+bikes = []
+bikes.append('redline')
+bikes.append('trek')
+print(bikes)
+squares = []
+for x in range(1,11):
+    squares.append(x**2)
+print(squares)
+first_two = names[:2]
+print(first_two)
+copy_of_names = names[:]
+print(copy_of_names)
+dimensions = (300, 500)
+print(dimensions)
