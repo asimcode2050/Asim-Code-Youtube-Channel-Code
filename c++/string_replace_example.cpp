0cf1edf144fe9dfee4ff9d7033bc06731d8aebd9
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-14 23:35:57 +0500

    Create string_replace_example.cpp

---
+++ c++/string_replace_example.cpp
@@ -0,0 +1,19 @@
+# https://youtu.be/uTVWrYS6Tfo
+#include <iostream>
+#include <string>
+using namespace std;
+
+string replaceStr(string orig_str, const string& replace_string, const string& with_string){
+    size_t pos = orig_str.find(replace_string);
+    if(pos != std::string::npos)
+        orig_str.replace(pos, replace_string.length(),with_string);
+    return orig_str;
+    }
+
+int main(){
+    string orig = "This is a testing";
+    string rep = "testing";
+    string wit ="test";
+    cout << replaceStr(orig,rep,wit) << endl;
+    return 0;
+}
