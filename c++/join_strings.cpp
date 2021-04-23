533822dd1e2a1b4e06f10a9160d95aa9df48243c
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-24 00:45:48 +0500

    Create join_strings.cpp

---
+++ c++/join_strings.cpp
@@ -0,0 +1,11 @@
+// https://youtu.be/Li_G6_KYqFg
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string s1 = "Asim";
+    std::string s2 = " Code";
+    std::string result = s1 + s2;
+    std::cout << "The resulting string is : " << result;
+    return 0;
+}
