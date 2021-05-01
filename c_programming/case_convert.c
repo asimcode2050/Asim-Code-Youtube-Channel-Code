f1a6a6846972bb9860c368dcf6d7bd7f324d1dc7
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-05-01 20:02:03 +0500

    Create case_convert.c

---
+++ c_programming/case_convert.c
@@ -0,0 +1,26 @@
+// https://youtu.be/JcDgdFrEsDo
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char myArray[20] = {0};
+    char newcase[20] = {0};
+    int i;
+    while(fgets(myArray,sizeof(myArray),stdin)!=NULL){
+        for(i=0;i<sizeof(myArray);i++){
+            //upper case to lower case
+            if((myArray[i] >= 65) && (myArray[i] <= 90)){
+                newcase[i] = myArray[i] + 32;
+            }
+            //lower case to upper case
+            if((myArray[i] >= 97 && myArray[i] <= 122)){
+                newcase[i] = myArray[i] - 32;
+            }
+        }
+        printf("%s\n",newcase);
+        memset(myArray,0,sizeof(myArray));
+        memset(newcase,0,sizeof(newcase));
+
+    }
+    return 0;
+}
