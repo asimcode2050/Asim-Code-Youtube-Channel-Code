ca09c7676db99b2dcfb73562e257de1e22681357
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-09-10 22:44:57 +0500

    Update test_pygithub.py

--- python/test_pygithub.py
+++ python/test_pygithub.py
@@ -1,15 +1,16 @@
 ''' Youtube Channel : Asim Code
 PyGithub repository example in Python
-https://youtu.be/gJPV8-vHDIE
+https://youtu.be/95Qu8icyjAk
 '''
 from github import Github
 my_git = Github("your_token")
 for repo in my_git.get_user().get_repos():
     print(repo.name)
 
-code_repo = my_git.get_repo("asimcode2050/Asim-Code-Youtube-Channel-Code")
-contents = code_repo.get_contents("")
+asim_code = my_git.get_repo("asimcode2050/Asim-Code-Youtube-Channel-Code")
+contents = asim_code.get_contents("")
 for content_file in contents:
     print(content_file)
+
     
 
