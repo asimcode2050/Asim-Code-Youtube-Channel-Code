dc5f3c62f0ff3311eeb309d7839e41ccae369d99
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-09-19 14:53:27 +0500

    Create os_system.js

---
+++ Node.js/os_system.js
@@ -0,0 +1,21 @@
+/*
+Node.js Operating system related utility functions
+https://youtu.be/TE1PpG3qwTY
+YouTube Channel: Asim Code
+*/
+const os = require('os');
+console.log(os.tmpdir());
+console.log(os.hostname());
+console.log(os.type());
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.release());
+console.log(os.uptime());
+console.log(os.loadavg());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.cpus());
+console.log(os.networkInterfaces());
+console.log(os.EOL);
+
+
