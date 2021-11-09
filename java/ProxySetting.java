afe9b93464ad73095aaf79140f984ce69899aeb1
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-11-10 01:11:04 +0500

    Create ProxySetting.java

---
+++ java/ProxySetting.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.InetSocketAddress;
+public class ProxySetting {
+    public static void main(String[] args) throws IOException, InterruptedException
+     {
+         HttpClient http_client = HttpClient.newBuilder().
+         proxy(ProxySelector.of(new InetSocketAddress("proxyhost",80))).build();
+
+         HttpRequest http_request = HttpRequest.newBuilder()
+         .uri(URI.create("https://github.com")).build();
+
+         HttpResponse<String> response = http_client.send(http_request,HttpResponse.BodyHandlers.ofString());
+         System.out.println("Status Code : "+response.statusCode());
+         System.out.println("\n Body: "+response.body());
+
+
+       
+
+    }
+}
