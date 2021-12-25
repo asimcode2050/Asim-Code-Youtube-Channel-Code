c383b9fe4f2e5770f0c1562852e2db85e45585e4
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-12-25 18:38:21 +0500

    Create arr_example.js

---
+++ java_script/arr_example.js
@@ -0,0 +1,10 @@
+let my_array = [];
+my_array[0] = "one";
+my_array[1] = "two";
+my_array.push("three");
+my_array.push("four");
+console.log(my_array);
+let arr2 = [1,2,3,4];
+delete arr2[3];
+console.log(arr2);
+      
