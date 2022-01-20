1f2a437a726fcbefd59c53ac71cd7e03004283ab
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2022-01-21 00:36:20 +0500

    Create seconds_to_hours_minutes_and_seconds.py

---
+++ python/seconds_to_hours_minutes_and_seconds.py
@@ -0,0 +1,8 @@
+"""
+Please Subscribe to Asim Code.
+Seconds to hours minutes seconds Time in Python
+YouTube Channel: Asim Code
+https://youtu.be/inO1wogHdc8
+"""
+import datetime
+print(str(datetime.timedelta(seconds=8274)))
