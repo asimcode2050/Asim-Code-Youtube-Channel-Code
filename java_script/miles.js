a4063ef506845f83a9df0560edab5bdf92bda0ff
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2022-10-30 01:26:01 +0500

    Update miles.js

--- java_script/miles.js
+++ java_script/miles.js
@@ -1,5 +1,5 @@
 let distance_miles = 150;
 //1 mile is equals to 1.60934 kilometers
-let distance_km = distance_miles * 1.60934
+let distance_km = distance_miles * 1.60934;
 console.log("The distance of "+distance_miles + " miles is equal to " +distance_km + " kilometers");
 
