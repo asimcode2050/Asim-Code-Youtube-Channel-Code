ef167719a1d622932ed1365ae93a21116539e55b
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-04-16 01:02:18 +0500

    Create link_list.cpp

---
+++ c_programming/link_list.cpp
@@ -0,0 +1,44 @@
+/*
+Singly linked list in C++
+https://youtu.be/PVAmSCbKsa0
+Youtube: Asim Code
+*/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+// HEAD ->[data][next]->[data][next]->[data][next]->NULL
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *head = NULL;
+
+// 3 -> 5 -> 2 -> NULL
+void insert(int data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = head;
+    head = new_node;
+}
+
+void printList()
+{
+    struct Node *ptr;
+    ptr = head;
+    while (ptr != NULL)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+}
+int main(void)
+{
+    insert(2);
+    insert(5);
+    insert(3);
+    printList();
+
+    return 0;
+}
