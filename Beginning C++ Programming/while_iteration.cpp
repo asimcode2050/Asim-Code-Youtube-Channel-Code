28103733d6ccb9cd8259fcc50ed0281624b29849
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-09-09 11:18:04 -0700

    Create while_iteration.cpp

---
+++ Beginning C++ Programming/while_iteration.cpp
@@ -0,0 +1,18 @@
+/*
+YouTube: Asim Code
+While iteration statement in C++
+https://youtu.be/Sv6l06WyAW0
+  */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count{1};
+    while (count <= 5)
+    {
+        cout << count << " ";
+        ++count;
+    }
+    cout << "\n";
+}
