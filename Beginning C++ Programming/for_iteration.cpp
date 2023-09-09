9e3a8acf61f9ceb5699a7dcfbfd8e7d3a379de3e
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-09-09 11:30:54 -0700

    Create for_iteration.cpp

---
+++ Beginning C++ Programming/for_iteration.cpp
@@ -0,0 +1,16 @@
+/*
+YouTube: Asim Code
+for iteration in C++
+https://youtu.be/55T1mJfWah8
+  */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int count{1}; count <= 10; ++count)
+    {
+        cout << count << " ";
+    }
+    cout << "\n";
+}
