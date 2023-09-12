68ae46afeb6c978a96e4a301bd2d9fa08a4348dd
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-09-12 09:19:01 -0700

    Create do_while.cpp

---
+++ Beginning C++ Programming/do_while.cpp
@@ -0,0 +1,19 @@
+/*
+YouTube: Asim Code
+Do While iteration statement in C++
+https://youtu.be/TaqeJxf-o14
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int counter{1};
+    do
+    {
+        cout << counter << " ";
+        ++counter;
+    } while (counter <= 5);
+    cout << "\n";
+}
