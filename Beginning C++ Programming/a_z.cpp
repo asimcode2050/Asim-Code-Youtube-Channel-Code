5e0fc12401829c63d3c47d284e983908c7095e8b
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-09-15 12:48:34 -0700

    Create a_z.cpp

---
+++ Beginning C++ Programming/a_z.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        cout << ch;
+    }
+    cout << endl;
+    return 0;
+}
