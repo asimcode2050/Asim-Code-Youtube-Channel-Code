8be05f00b5b128c09a6fa4bfbd52cb10c0eb838f
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2023-09-15 14:30:04 -0700

    Create gardes_first_second.cpp

---
+++ Beginning C++ Programming/gardes_first_second.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int student_marks, number_of_students;
+    double marks1 = -1;
+    double marks2 = -2;
+    cout << "Please enter the number of students: ";
+    cin >> number_of_students;
+    for (int i = 0; i < number_of_students; i++)
+    {
+        cout << "Enter a student marks: ";
+        cin >> student_marks;
+        if (student_marks > marks1)
+        {
+            marks2 = marks1;
+            marks1 = student_marks;
+        }
+        else if (student_marks > marks2)
+        {
+            marks2 = student_marks;
+        }
+    }
+    cout << "Highest Student marks is : " << marks1 << endl;
+    cout << "Second Highest Student marks is : " << marks2 << endl;
+
+    return 0;
+}
