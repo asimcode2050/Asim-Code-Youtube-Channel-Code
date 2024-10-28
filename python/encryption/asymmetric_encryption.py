357f210817848edec5a15165021dcff24ae067e8
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-10-28 12:54:36 -0700

    Create asymmetric_encryption.py

---
+++ python/encryption/asymmetric_encryption.py
@@ -0,0 +1,42 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+public_key = private_key.public_key()
+
+data = b"This is some data to be signed."
+signature = private_key.sign(
+    data,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+
+try:
+    public_key.verify(
+        signature,
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+
+        ),
+        hashes.SHA256()
+    )
+
+    print("Signature is valid.")
+except Exception as e:
+    print("Signature verification failed:", str(e))
+
