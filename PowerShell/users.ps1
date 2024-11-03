c51b3329e409357ab1b3808c78d9fc8332de9c8d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-03 10:29:00 -0800

    Create users.ps1

---
+++ PowerShell/users.ps1
@@ -0,0 +1,4 @@
+Get-WmiObject -Class Win32_UserAccount
+Get-LocalUser | ft Name,Enabled,LastLogon
+Get-ChildItem C:\Users -Force | select Name
+Get-LocalGroupMember Administrators | ft Name, PrincipalSource
