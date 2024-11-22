29b185767edbaa74a025b3b7290fb37e8253e52e
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-22 10:22:38 -0800

    Create operators.js

---
+++ learn_javascript/operators.js
@@ -0,0 +1,38 @@
+let a = 10;
+let b = 5;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+let remainder = a % b;
+let x = 3;
+x += 2; // Equivalent to x = x + 2
+x -= 1; // Equivalent to x = x - 1
+x *= 4; // Equivalent to x = x * 4
+x /= 2; // Equivalent to x = x / 2
+x %= 3; // Equivalent to x = x % 3
+let x = 10;
+let y = 20;
+let isEqual = x == y;
+let isNotEqual = x != y;
+let isGreaterThan = x > y;
+let isLessThan = x < y;
+let isGreaterOrEqual = x >= y;
+let isLessOrEqual = x <= y;
+let a = true;
+let b = false;
+let and = a && b;
+let or = a || b;
+let not = !a;
+let age = 18;
+let canVote = age >= 18 ? 'Yes' : 'No';
+let counter = 0;
+counter++;
+counter--;
+let num = 42;
+let isNumber = typeof num;
+let isUndefined;
+let isUndefinedType = typeof isUndefined;
+let firstName = 'John';
+let lastName = 'Doe';
+let fullName = firstName + ' ' + lastName; 
