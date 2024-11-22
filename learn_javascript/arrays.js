bf1c569b030012e1f90faa78fcd6b359deb3871b
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-22 13:22:19 -0800

    Update arrays.js

--- learn_javascript/arrays.js
+++ learn_javascript/arrays.js
@@ -1,1 +1,27 @@
+let fruits = ["apple", "banana", "cherry"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+
+fruits.push("date");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits); // Now the array is ["apple", "banana", "cherry"]
+
+fruits.unshift("apricot");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+let index = fruits.indexOf("banana");
+console.log(index);
+
+let hasCherry = fruits.includes("cherry");
+console.log(hasCherry);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
 
