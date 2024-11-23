607a6e3795a738619949ced84e7f5464804ed263
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-23 00:40:56 -0800

    Create switch_statement.js

---
+++ learn_javascript/switch_statement.js
@@ -0,0 +1,27 @@
+let dayOfWeek = 3; // 1 = Sunday, 2 = Monday, 3 = Tuesday, etc.
+switch (dayOfWeek) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
