5bb898080e5ec62e98e3eb6ab8e232815716e56c
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-23 09:57:11 -0800

    Create arrow.js

---
+++ learn_javascript/arrow.js
@@ -0,0 +1,9 @@
+const add = (a, b) => {
+    return a + b;
+};
+let result = add(5, 10);
+console.log(result);  // The expected output is 15
+const double = x => x * 2;
+let doubledValue = double(4);
+console.log(doubledValue);  // This will output 8.
+
