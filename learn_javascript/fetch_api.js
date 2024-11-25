fe34159853e849d0df53e3f8cd25ba4fd6337a3d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-25 11:08:54 -0800

    Create fetch_api.js

---
+++ learn_javascript/fetch_api.js
@@ -0,0 +1,13 @@
+fetch('https://jsonplaceholder.typicode.com/posts')  // The URL we are sending a request to.
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
