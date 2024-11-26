28798c1a5287cfbee2ecacc98d99c8d38af294d6
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-26 09:05:10 -0800

    Create casting.py

---
+++ learn_python/casting.py
@@ -0,0 +1,12 @@
+age = "25"
+price = 19.99
+age_int = int(age)
+price_int = int(price)
+price_float = float(price_int)
+age_str = str(age_int)
+print("Original age (as string):", age)
+print("Converted age (as integer):", age_int)
+print("Original price (as float):", price)
+print("Converted price (as integer):", price_int)
+print("Converted price (back to float):", price_float)
+print("Converted age (back to string):", age_str)
