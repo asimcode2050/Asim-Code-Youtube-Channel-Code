dc698cac2baa35604c9ea518ad22ad4322c597f2
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-27 01:18:08 -0800

    Create ifelse.py

---
+++ learn_python/ifelse.py
@@ -0,0 +1,7 @@
+age = 17
+if age >= 18:
+    print("You are an adult.")
+elif age >= 13:
+    print("You are a teenager.")
+else:
+    print("You are a child.")  # This message prints if age is less than 13.
