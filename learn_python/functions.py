5ecf81eaba8b22a43ff0c491a1bc6c387f5f5832
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-11-27 10:16:45 -0800

    Create functions.py

---
+++ learn_python/functions.py
@@ -0,0 +1,8 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+length_of_rectangle = 5
+width_of_rectangle = 3
+calculated_area = calculate_area(length_of_rectangle, width_of_rectangle)
+print("The area of the rectangle is:", calculated_area)
