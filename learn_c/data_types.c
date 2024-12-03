189203ecbe25849cdb85ed70a90ed8a3b6029236
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-03 08:27:29 -0800

    Create data_types.c

---
+++ learn_c/data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int integerVar = 10;
+    char charVar = 'A';
+    float floatVar = 3.14f;
+    double doubleVar = 3.14159;
+    printf("Integer: %d\n", integerVar);
+    printf("Character: %c\n", charVar);
+    printf("Float: %.2f\n", floatVar);
+    printf("Double: %.5f\n", doubleVar);
+    return 0;
+}
