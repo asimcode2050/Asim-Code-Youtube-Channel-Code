8e99fe580c77bdbe353a1593ee32732338c1b586
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-04 00:08:03 -0800

    Create ifelse_ex.c

---
+++ learn_c/ifelse_ex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a = 15;
+    int b = 20;
+    if (a > b)
+    {
+        printf("a is greater than b.\n");
+    }
+    else if (a < b)
+    {
+        printf("a is less than b.\n");
+    }
+    else
+    {
+        printf("a is equal to b.\n");
+    }
+    return 0;
+}
