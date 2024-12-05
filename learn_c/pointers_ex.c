2ddee487f9d7302860cd787f64a5e5ab0efd0383
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-05 08:06:06 -0800

    Create pointers_ex.c

---
+++ learn_c/pointers_ex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+    int x = 5;
+    int y = 10;
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y); // 'x' and 'y' are passed by reference (example, using their memory addresses)
+    printf("After swap: x = %d, y = %d\n", x, y);
+    return 0;
+}
