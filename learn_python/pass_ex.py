bbff47529173f1f493ba68960e01bd2fc97184e8
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-13 08:23:55 -0800

    Create pass_ex.py

---
+++ learn_python/pass_ex.py
@@ -0,0 +1,17 @@
+def placeholder_function():
+    pass
+
+x = 10
+if x > 5:
+    pass
+else:
+    print("x is less than or equal to 5")
+for i in range(5):
+    pass
+'''
+ Why we use 'pass':
+ - It is a placeholder.
+ - It allows us to write syntactically correct code, even when we are not ready to define the behavior yet.
+ - It can be used to avoid errors in empty function definitions, loops, or conditionals.
+ - It helps us plan and organize our code structure, while leaving some parts to be filled in later.
+ '''
