9b396871ef94542bc763af32ee22300adc270394
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-14 01:09:18 -0800

    Create del_ex.py

---
+++ learn_python/del_ex.py
@@ -0,0 +1,3 @@
+shopping_list = ["apples", "bread", "milk", "cookies"]
+del shopping_list[3]  # del statement to remove item at index 3 (cookies)
+print(shopping_list)  # print function to display the list, no parameters
