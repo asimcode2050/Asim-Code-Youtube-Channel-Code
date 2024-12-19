95e4e30a11815f8808674113797e0fe3f07bb84a
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-19 01:42:46 -0800

    Create Main.java

---
+++ learn_java/Main.java
@@ -0,0 +1,12 @@
+/* System.out.println("Hello, World!");
+*/
+/* System.out.print("Hello, "); */
+public class Main { 
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Welcome to Java programming.");
+        System.out.println(" This is on a new line.");
+    }
+}  
+
