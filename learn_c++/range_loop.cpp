26bc7caf5f934698e25ccc2ba741a2e9770334b2
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2024-12-22 12:53:27 -0800

    Create range_loop.cpp

---
+++ learn_c++/range_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    for (int number : numbers)
+    {
+        std::cout << number << " ";
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
