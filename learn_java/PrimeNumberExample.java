5a23ee8555bb81244f5438271658ec1ea115cd6d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2025-01-09 11:32:58 -0800

    Create PrimeNumberExample.java

---
+++ learn_java/PrimeNumberExample.java
@@ -0,0 +1,31 @@
+
+public class PrimeNumberExample {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printPrimes(int limit) {
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int limit = 100;
+        printPrimes(limit);
+    }
+}
