032fc6f5f9769e3bfd4ed7fed5cb5adacc01d301
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2025-01-15 08:30:06 -0800

    Create student_p.py

---
+++ python/machine_learning/student_p.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+data = pd.read_csv(r'C:\asim\cyber\python\student_data.csv')
+
+print(data.head())
+
+X = data[['study_hours', 'attendance', 'previous_grades', 'parent_income']]
+
+y = data['pass_fail']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler() 
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
